1e294eb908d6b42f332142949ee144c8c50a40a5
Aaron Parker <aaron@stealthpuppy.com>
2017-05-20 13:14:35 +1000

    Clean up cmdlets, spelling etc.

--- Get-LatestUpdate.ps1
+++ Get-LatestUpdate.ps1
@@ -1,66 +1,71 @@
 <#
 .SYNOPSIS
-Get the latest Cumulative update for Windows
+    Get the latest Cumulative update for Windows
 
 .DESCRIPTION
-This script will return a list of updates based on a search string
+    This script will return the list of Cumulative updates for Windows 10 and Windows Server 2016 from the Microsoft Update Catalog.
 
 .NOTES
-Copyright Keith Garner, All rights reserved.
-Forked from: https://gist.github.com/keithga/1ad0abd1f7ba6e2f8aff63d94ab03048
+    Name: Install-VisualCRedistributables.ps1
+    Author: Aaron Parker
+    Twitter: @stealthpuppy
+    
+    Original script:
+    Copyright Keith Garner, All rights reserved.
+    Forked from: https://gist.github.com/keithga/1ad0abd1f7ba6e2f8aff63d94ab03048
 
 .LINK
-https://support.microsoft.com/en-us/help/4000823
+    https://support.microsoft.com/en-us/help/4000823
 
 .EXAMPLE
+    Get the latest Cumulative Update for Windows 10 x64
 
-Get the latest Windows 10 Cumulative Update x64 (this is the default)
-
-.\get-latestUpdate.ps1 
-
-.EXAMPLE
-
-Get the latest Windows 10 Cumulative Update x86
-
-.\get-latestUpdate.ps1 -SearchString 'Cumulative.*x86'
+    .\Get-LatestUpdate.ps1 
 
 .EXAMPLE
+    Get the latest Cumulative Update for Windows 10 x86
 
-Get the latest Windows Server 2016 Cumulative Update x64 
+    .\Get-LatestUpdate.ps1 -SearchString 'Cumulative.*x86'
 
-.\get-latestUpdate.ps1 -SearchString 'Cumulative.*Server.*x64' -OSBuild 14393
+.EXAMPLE
+    Get the latest Cumulative Update for Windows Server 2016
 
-
+    .\Get-LatestUpdate.ps1 -SearchString 'Cumulative.*Server.*x64' -Build 14393
 #>
+[CmdletBinding(SupportsShouldProcess = $True, ConfirmImpact = "Low", DefaultParameterSetName='Base')]
+Param(
+    [Parameter(ParameterSetName='Base', Mandatory=$False, HelpMessage="JSON source for the update KB articles.")]
+    [string]$startKB = 'https://support.microsoft.com/api/content/asset/4000816',
 
-[cmdletbinding()]
-param(
-    [string] $StartKB = 'https://support.microsoft.com/api/content/asset/4000816',
+    [Parameter(ParameterSetName='Base', Mandatory=$False, HelpMessage="Windows build number.")]
     [ValidateSet('15063','14393','10586','10240')]
-    [string] $OSBuild = '15063',
-    [string] $SearchString = 'Cumulative.*x64'
+    [string]$Build = '15063',
+
+    [Parameter(ParameterSetName='Base', Mandatory=$False, HelpMessage="Search query string.")]
+    [string]$SearchString = 'Cumulative.*x64',
+
+    [Parameter(ParameterSetName='Base', Mandatory=$False, HelpMessage="Download the discovered updates.")]
+    [switch]$Download = $False
 )
 
 #region Support Routine
-
-function Find-LatestUpdate {
+Function Find-LatestUpdate {
     [CmdletBinding(SupportsShouldProcess=$True)]
-    param(
-        [parameter(Mandatory=$true, 
-        ValueFromPipeline=$true)]
+    Param(
+        [parameter(Mandatory=$True, ValueFromPipeline=$True)]
         $Updates
     )
     Begin { 
-        $MaxObject = $null
-        $MaxValue = [version]::new("0.0")
+        $maxObject = $Null
+        $maxValue = [version]::new("0.0")
     }
     Process {
-        foreach ( $Update in $updates ) {
+        ForEach ( $Update in $Updates ) {
             Select-String -InputObject $Update -AllMatches -Pattern "(\d+\.)?(\d+\.)?(\d+\.)?(\*|\d+)" |
             ForEach-Object { $_.matches.value } |
             ForEach-Object { $_ -as [version] } |
             ForEach-Object { 
-                if ( $_ -gt $MaxValue ) { $MaxObject = $Update; $MaxValue = $_ }
+                If ( $_ -gt $MaxValue ) { $MaxObject = $Update; $MaxValue = $_ }
             }
         }
     }
@@ -68,49 +73,49 @@ function Find-LatestUpdate {
         $MaxObject | Write-Output 
     }
 }
-
 #endregion
 
 #region Find the KB Article Number
-
-write-verbose "download $StartKB and get a list of Updates"
-$KBID = iwr $StartKB |
+Write-Verbose "Downloading $startKB to retrieve the list of updates."
+$kbID = Invoke-WebRequest $startKB |
     Select-Object -ExpandProperty Content |
     ConvertFrom-Json |
     Select-Object -ExpandProperty Links |
     Where-Object level -eq 2 |
-    where-object text -match $OSBuild |
+    Where-Object text -match $Build |
     Find-LatestUpdate |
     Select-Object -First 1
-
 #endregion
 
 #region get the download link from Windows Update
+Write-Verbose "Found kbID: http://www.catalog.update.microsoft.com/Search.aspx?q=KB$($kbID.articleID)"
+$kbObj = Invoke-WebRequest "http://www.catalog.update.microsoft.com/Search.aspx?q=KB$($kbID.articleID)" 
 
-write-verbose "Found KBID: http://www.catalog.update.microsoft.com/Search.aspx?q=KB$($KBID.articleID)"
-$KBObj = iwr "http://www.catalog.update.microsoft.com/Search.aspx?q=KB$($KBID.articleID)" 
-
-$Available_KBIDs = $KBObj.InputFields | 
-    where-object { $_.type -eq 'Button' -and $_.Value -eq 'Download' } | 
+$Available_kbIDs = $kbObj.InputFields | 
+    Where-Object { $_.Type -eq 'Button' -and $_.Value -eq 'Download' } | 
     Select-Object -ExpandProperty  ID
 
-$Available_KBIDs | out-string | write-verbose
+$Available_kbIDs | Out-String | Write-Verbose
 
-$KBIDs = $KBObj.Links | 
-    where-object ID -match '_link' |
+$kbIDs = $kbObj.Links | 
+    Where-Object ID -match '_link' |
     Where-Object InnerText -match $SearchString |
-    ForEach-Object { $_.id.replace('_link','') } |
-    Where-Object { $_ -in $Available_KBIDs }
+    ForEach-Object { $_.Id.Replace('_link','') } |
+    Where-Object { $_ -In $Available_kbIDs }
 
-foreach ( $KBID in $KBIDs )
+ForEach ( $kbID in $kbIDs )
 {
-    Write-Verbose "`t`tDownload $KBID"
-    $Post = @{ size = 0; updateID = $KBID; uidInfo = $KBID } | ConvertTo-Json -Compress
+    Write-Verbose "`t`tDownload $kbID"
+    $Post = @{ size = 0; updateID = $kbID; uidInfo = $kbID } | ConvertTo-Json -Compress
     $PostBody = @{ updateIDs = "[$Post]" } 
     Invoke-WebRequest -Uri 'http://www.catalog.update.microsoft.com/DownloadDialog.aspx'  -Method Post -Body $postBody |
         Select-Object -ExpandProperty Content |
-        select-string -AllMatches -Pattern "(http[s]?\://download\.windowsupdate\.com\/[^\'\""]*)" | 
+        Select-String -AllMatches -Pattern "(http[s]?\://download\.windowsupdate\.com\/[^\'\""]*)" | 
         ForEach-Object { $_.matches.value }
 }
-
 #endregion
+
+
+If ($pscmdlet.ShouldProcess($kbID, "Downloading")) {
+
+}
